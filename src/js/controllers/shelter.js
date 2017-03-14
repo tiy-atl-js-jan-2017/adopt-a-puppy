b0f05d9b88e171a822c9dcadaeeac4dbf310b602
Brit Butler <redline6561@gmail.com>
2017-03-14 11:56:50 -0400

    Add a way to create shelters on the backend.

--- src/js/controllers/shelter.js
+++ src/js/controllers/shelter.js
@@ -1,5 +1,19 @@
-function ShelterController () {
+const SERVER_URL = "https://enigmatic-cliffs-77454.herokuapp.com";
+
+function ShelterController ($scope, $http) {
   console.log("Welcome to Shelter controller");
+  $scope.currentShelter = null;
+
+  $scope.addShelter = function (data) {
+    // console.log(shelter);
+
+    $http.post(`${SERVER_URL}/shelters`, data).then(resp => {
+      console.log(resp.data);
+      $scope.currentShelter = resp.data;
+    });
+  };
 }
+
+ShelterController.$inject = ['$scope', '$http'];
 
 export default ShelterController;
