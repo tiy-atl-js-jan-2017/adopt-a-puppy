4136c4afe570007bbd81e5e84e721bf47d2d299a
Brit Butler <redline6561@gmail.com>
2017-03-14 12:17:42 -0400

    Add ability to add puppies and show puppies from a shelter on homepage.

--- src/js/controllers/animal.js
+++ src/js/controllers/animal.js
@@ -1,5 +1,29 @@
-function AnimalController () {
+const SERVER_URL = "https://enigmatic-cliffs-77454.herokuapp.com";
+
+function AnimalController ($scope, $http) {
   console.log("Welcome to Animal controller");
+  $scope.currentPuppy = null;
+  $scope.puppies = [];
+
+  function init () {
+    $http.get(`${SERVER_URL}/shelters/1`).then(resp => {
+      $scope.puppies = resp.data;
+    });
+  }
+
+  init();
+
+  $scope.addPuppy = function (data, shelterId) {
+    var url = `${SERVER_URL}/shelters/${shelterId}/animals`;
+    $http.post(url, data).then(resp => {
+      console.log(resp.data);
+      $scope.currentPuppy = resp.data;
+    });
+  };
+
+
 }
+
+AnimalController.$inject = ['$scope', '$http'];
 
 export default AnimalController;
