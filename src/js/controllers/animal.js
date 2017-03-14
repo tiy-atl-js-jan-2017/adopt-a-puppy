dd27db0ef149e03708a6003a5aec0fd2bf2d7bda
Brit Butler <redline6561@gmail.com>
2017-03-14 12:24:36 -0400

    Add new puppies to the list so the page updates. Also fix image sizing.

--- src/js/controllers/animal.js
+++ src/js/controllers/animal.js
@@ -18,6 +18,7 @@ function AnimalController ($scope, $http) {
     $http.post(url, data).then(resp => {
       console.log(resp.data);
       $scope.currentPuppy = resp.data;
+      $scope.puppies.push(resp.data);
     });
   };
 
