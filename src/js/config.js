55dc9f199ae38ce7a91a0b51e88cf73ae8eeb54b
Brit Butler <redline6561@gmail.com>
2017-03-15 09:47:57 -0400

    Set Home as default state and add an error state.

--- src/js/config.js
+++ src/js/config.js
@@ -6,7 +6,14 @@ function Config ($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/home',
       template: '<h1>Hello World</h1>'
+    })
+    .state('page-not-found', {
+      url: '/not-found',
+      template: `<h2>No such page. Haha. You're dumb.</h2>`
     });
+
+  $urlRouterProvider.when('', '/home');
+  $urlRouterProvider.otherwise('/not-found');
 };
 
 Config.$inject = ['$stateProvider', '$urlRouterProvider'];
