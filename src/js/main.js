ba8f3cbe90c044ca397a37625bb2c57ea45b9d7b
Brit Butler <redline6561@gmail.com>
2017-03-15 09:19:14 -0400

    Add Angular UI Router.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 import angular from 'angular';
+import 'angular-ui-router';
 
 import AnimalController from './controllers/animal';
 import ShelterController from './controllers/shelter';
 
 angular
-  .module('app', [])
+  .module('app', ['ui.router'])
   .controller('ShelterController', ShelterController)
   .controller('AnimalController', AnimalController);
