b682de1e71601119e536b7d557fcac350ce68098
Brit Butler <redline6561@gmail.com>
2017-03-15 10:49:34 -0400

    Create a new state for shelter add page.

--- src/js/config.js
+++ src/js/config.js
@@ -7,6 +7,15 @@ function Config ($stateProvider, $urlRouterProvider) {
       url: '/home',
       template: '<h1>Hello World</h1>'
     })
+    .state('shelter-new', {
+      url: '/shelters/new',
+      templateUrl: 'templates/shelter-add.tpl.html',
+      controller: 'ShelterController'
+    })
+    .state('shelter', {
+      url: '/shelters',
+      template: `<h1>Shelters are cool cause animals are cool.</h1>`
+    })
     .state('page-not-found', {
       url: '/not-found',
       template: `<h2>No such page. Haha. You're dumb.</h2>`
