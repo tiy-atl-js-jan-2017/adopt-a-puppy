ccf7a54f21fe5d036da8cd39b56474263868b47a
Brit Butler <redline6561@gmail.com>
2017-03-16 12:03:05 -0400

    Show one option for passing headers.

--- src/js/controllers/animal-add.js
+++ src/js/controllers/animal-add.js
@@ -1,14 +1,21 @@
-function AnimalAddController ($scope, $http, $state, SERVER) {
+function AnimalAddController ($scope, $http, $state, SERVER, $cookies) {
 
   $scope.addPuppy = function (data, shelterId) {
     var url = `${SERVER}/shelters/${shelterId}/animals`;
-    $http.post(url, data).then(resp => {
+    $http({
+      url: url,
+      method: 'POST',
+      headers: {
+        'access-token': $cookies.get('access-token')
+      },
+      data: data
+    }).then(resp => {
       $state.go('root.animals', { id: shelterId });
     });
   };
 
 };
 
-AnimalAddController.$inject = ['$scope', '$http', '$state', 'SERVER'];
+AnimalAddController.$inject = ['$scope', '$http', '$state', 'SERVER', '$cookies'];
 
 export default AnimalAddController;
