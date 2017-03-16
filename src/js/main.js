003fd9fd32c32c5efe90b9638f4904eefdefe384
Brit Butler <redline6561@gmail.com>
2017-03-16 10:59:32 -0400

    Add ngCookies to the project.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import 'angular-ui-router';
+import 'angular-cookies';
 
 import AppConfig from './config';
 import SERVER from './server';
@@ -10,7 +11,7 @@ import ShelterController from './controllers/shelter';
 import UserController from './controllers/user';
 
 angular
-  .module('app', ['ui.router'])
+  .module('app', ['ui.router', 'ngCookies'])
   .config(AppConfig)
   .constant('SERVER', SERVER)
   .controller('UserController', UserController)
